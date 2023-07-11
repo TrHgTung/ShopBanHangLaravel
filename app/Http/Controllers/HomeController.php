4cebf726884e539395473ef1629ccab58ec72ec4
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-07-11 20:17:20 +0700

    query brand name (6:50 - bai27)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use Session;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Redirect;
+session_start();
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $cate_product = DB::table('tbl_category_product')->orderby('category_id','desc')->get();
-        $brand_product = DB::table('tbl_brand_product')->orderby('brand_id','desc')->get();
+        $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get();
+        $brand_product = DB::table('tbl_brand_product')->where('brand_status','0')->orderby('brand_id','desc')->get();
 
-        return view('pages.home');
+        return view('pages.home')->with('category', $cate_product)->with('brand', $brand_product);
 
     }
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -210,22 +210,22 @@
 					<div class="left-sidebar">
 						<h2>Danh mục sản phẩm</h2>
 						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
-
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Kids</a></h4>
+							@foreach($category as $key => $cate)
+								<div class="panel panel-default">
+									<div class="panel-heading">
+										<h4 class="panel-title"><a href="#">Kids</a></h4>
+									</div>
 								</div>
-							</div>
-							
+							@endforeach
 						</div><!--/category-products-->
 					
 						<div class="brands_products"><!--brands_products-->
 							<h2>Thương hiệu</h2>
 							<div class="brands-name">
 								<ul class="nav nav-pills nav-stacked">
-									
-									<li><a href="#"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
-								
+									@foreach($brand as $key => $brand)
+										<li><a href="#"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
+									@endforeach
 								</ul>
 							</div>
 						</div><!--/brands_products-->
