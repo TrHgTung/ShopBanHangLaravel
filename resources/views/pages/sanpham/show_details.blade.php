a365250b8f6bc4aa7f910c655b5cac7848b0197c
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-07-19 15:11:35 +0700

    added show time function (JS) on show details
    Van la 7p50 bai30

--- resources/views/pages/sanpham/show_details.blade.php
+++ resources/views/pages/sanpham/show_details.blade.php
@@ -27,8 +27,9 @@
 								<p>Mã sản phẩm: {{$value->product_id}}</p>
 								<img src="images/product-details/rating.png" alt="" />
 								<span>
-									<span>US $59</span>
-									<label>Quantity:</label>
+									<span>Giá: {{number_format($value->product_price)}} VND</span>
+									<label>Số lượng có sẵn: {{$value->product_quantity}}</label>
+									<span>Mô tả ngắn gọn: {{$value->product_desc}}</span>
 									<input type="text" value="3" />
 									<button type="button" class="btn btn-fefault cart">
 										<i class="fa fa-shopping-cart"></i>
@@ -37,7 +38,7 @@
 								</span>
 								<p><b>Availability:</b> In Stock</p>
 								<p><b>Condition:</b> New</p>
-								<p><b>Brand:</b> E-SHOPPER</p>
+								<p><b>Brand</b> (brand_id): {{$value->brand_id}}</p>
 								<a href=""><img src="images/product-details/share.png" class="share img-responsive"  alt="" /></a>
 							</div><!--/product-information-->
 						</div>
@@ -53,15 +54,14 @@
 						</div>
 						<div class="tab-content">
 							<div class="tab-pane fade" id="details" >
-                                <p>Mô tả chi tiết mô tả chi tiết mô tả chi tiếtmô tả chi tiếtmô tả chi tiếtmô tả chi tiếtmô tả chi tiết
-                                        mô tả chi tiếtmô tả chi tiếtmô tả chi tiếtmô tả chi tiếtmô tả chi tiết</p>
+                                <p>{{$value->product_content}}</p>
 							</div>
 							<div class="tab-pane fade active in" id="reviews" >
 								<div class="col-sm-12">
                                 <ul>
 									<li>LẦN CUỐI CẬP NHẬT:</a></li>
-									<li class=""><a href="#"><i class="fa fa-clock-o"></i>12:41 PM</a></li>
-									<li><a href="#"><i class="fa fa-calendar-o"></i>31 DEC 2014</a></li>
+									<li class=""><a href="#" id="hienthithoigian"><i class="fa fa-clock-o"></i>12:41 PM</a></li>
+									<!-- <li><a href="#"><i class="fa fa-calendar-o"></i>31 DEC 2014</a></li> -->
 								</ul>
 									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 									<p><b>Write Your Review</b></p>
@@ -79,6 +79,28 @@
 									</form>
 								</div>
 							</div>
+							<script>
+								function hienThiThoiGianThuc() {
+									var date = new Date(); // Tạo một đối tượng Date mới
+									
+									var gio = date.getHours(); // Lấy giờ hiện tại
+									var phut = date.getMinutes(); // Lấy phút hiện tại
+									var giay = date.getSeconds(); // Lấy giây hiện tại
+									
+									// Định dạng lại chuỗi để hiển thị theo định dạng hh:mm:ss
+									var gioFormatted = gio < 10 ? "0" + gio : gio;
+									var phutFormatted = phut < 10 ? "0" + phut : phut;
+									var giayFormatted = giay < 10 ? "0" + giay : giay;
+									
+									var thoiGian = gioFormatted + ":" + phutFormatted + ":" + giayFormatted;
+									
+									// console.log(thoiGian); // In thời gian ra console
+									var outputElement = document.getElementById("hienthithoigian"); // Truy cập phần tử có id "output"
+  									outputElement.textContent = thoiGian;
+								}
+
+								hienThiThoiGianThuc();
+							</script>
 							
 						</div>
 					</div><!--/category-tab-->
