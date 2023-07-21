39be403d78b0a93a733ff4fa5a71f9156f6807cd
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-07-21 20:09:22 +0700

    fixed Index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,6 +17,10 @@ class HomeController extends Controller
         $brand_product = DB::table('tbl_brand_product')->where('brand_status','1')->orderby('brand_id','desc')->get();
 
         $all_product = DB::table('tbl_product')->where('product_status','1')->orderby('product_id','desc')->limit(4)->get();
+        // them
+        // $cate_product = DB::table('tbl_category_product')->where('category_status','1')->orderby('category_id','desc')->get();
+        // $brand_product = DB::table('tbl_brand_product')->where('brand_status','1')->orderby('brand_id','desc')->get();
+        // $details_product = DB::table('tbl_product')->join('tbl_category_product','tbl_category_product.category_id','=','tbl_product.category_id')->join('tbl_brand_product','tbl_brand_product.brand_id','=','tbl_product.brand_id')->where('tbl_product.product_id', $product_id)->limit(1)->get();
 
         return view('pages.home')->with('category_product' , $cate_product)->with('brand_product' , $brand_product)->with('all_product', $all_product);
         
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,25 +1,28 @@
 @extends('layout')
 @section('content')
 
-<div class="features_items"><!--features_items-->
-	<h2 class="title text-center">Sản phẩm mới</h2>
-	@foreach($all_product as $key => $product)
+<div class="features_items">
+	<h2 class="title text-center">Về chúng tôi</h2>
+	<p>Images carousel > about us > slider (about / store / history / which sell for ...)</p>
+	<!-- @foreach($all_product as $key => $product)
 		<div class="col-sm-4">
 			<div class="product-image-wrapper">
-				<div class="single-products">
-					<div class="productinfo text-center">
-						<img src="{{URL::to('public/upload/product/'.$product->product_image)}}" alt="" />
-						<h2>{{$product->product_name}}</h2>
-						<p>Giá bán: {{number_format($product->product_price)}} VND</p>
-					</div>
-					<div class="product-overlay">
-						<div class="overlay-content">
+				<a href="">
+					<div class="single-products">
+						<div class="productinfo text-center">
+							<img src="{{URL::to('public/upload/product/'.$product->product_image)}}" alt="" />
 							<h2>{{$product->product_name}}</h2>
-							<p>{{number_format($product->product_price)}} VND</p>
-							<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
+							<p>Giá bán: {{number_format($product->product_price)}} VND</p>
+						</div>
+						<div class="product-overlay">
+							<div class="overlay-content">
+								<h2>{{$product->product_name}}</h2>
+								<p>{{number_format($product->product_price)}} VND</p>
+								<a href="{{URL::to('/')}}" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Thêm vào giỏ hàng</a>
+							</div>
 						</div>
 					</div>
-				</div>
+				</a>
 				<div class="choose">
 					<ul class="nav nav-pills nav-justified">
 						<li><a href="#"><i class="fa fa-plus-square"></i>Yêu thích</a></li>
@@ -28,78 +31,8 @@
 				</div>
 	    	</div>
 		</div>
-	@endforeach
+	@endforeach -->
 						
-</div><!--features_items-->
+</div>
 
-                    <div class="recommended_items"><!--recommended_items-->
-						<h2 class="title text-center">recommended items</h2>
-						
-						<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
-							<div class="carousel-inner">
-								<div class="item active">
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="{{('public/frontend/images/recommend3.jpg')}}" alt="" />
-													<h2>$56</h2>
-													<p>Easy Polo Black Edition</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="item">	
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="{{('public/frontend/images/recommend1.jpg')}}" alt="" />
-													<h2>$56</h2>
-													<p>Easy Polo Black Edition</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="{{('public/frontend/images/recommend2.jpg')}}" alt="" />
-													<h2>$56</h2>
-													<p>Easy Polo Black Edition</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-									<div class="col-sm-4">
-										<div class="product-image-wrapper">
-											<div class="single-products">
-												<div class="productinfo text-center">
-													<img src="{{('public/frontend/images/recommend3.jpg')}}" alt="" />
-													<h2>$56</h2>
-													<p>Easy Polo Black Edition</p>
-													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-												</div>
-												
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							 <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
-								<i class="fa fa-angle-left"></i>
-							  </a>
-							  <a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
-								<i class="fa fa-angle-right"></i>
-							  </a>			
-						</div>
-					</div><!--/recommended_items-->
 @endsection
