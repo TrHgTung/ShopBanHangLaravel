1bdc832be796a58c7f936195dd77b46d4a457ead
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-11-07 10:03:42 +0700

    upd admin login interface

--- resources/views/admin/auth/admin_login.blade.php
+++ resources/views/admin/auth/admin_login.blade.php
@@ -41,12 +41,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			{{ csrf_field() }}
 			<input type="text" class="ggg" name="admin_email" placeholder="E-MAIL" required="">
 			<input type="password" class="ggg" name="admin_password" placeholder="PASSWORD" required="">
-			<span><input type="checkbox" />Remember Me</span>
-			<h6><a href="#">Forgot Password?</a></h6>
+			<!-- <span><input type="checkbox" />Remember Me</span>
+			<h6><a href="#">Forgot Password?</a></h6> -->
 				<div class="clearfix"></div>
 				<input type="submit" value="Sign In" name="login">
 		</form>
-		<p>Don't Have an Account ?<a href="registration.html">Create an account</a></p>
+		<!-- <p>Don't Have an Account ?<a href="registration.html">Create an account</a></p> -->
 </div>
 </div>
 <script src="{{asset('public/backend/js/bootstrap.js')}}"></script>
