e677769a210e17400107010a46a8f8e8ab504b7d
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-11-23 13:15:24 +0700

    update promotion & added Wheel

--- resources/views/pages/promotion/index.blade.php
+++ resources/views/pages/promotion/index.blade.php
@@ -4,6 +4,12 @@
 	
 <section id="slider"><!--slider-->
 	<div class="left-sidebar mt-4">
+			<h2>VÒNG QUAY MAY MẮN</h2>
+		</div>
+		<div class="container mt-4">
+			<a href="{{('wheel/wheel-index.html')}}" target="_blank"><img src="{{asset('public/frontend/images/wheel.jpg')}}" alt="" srcset=""></a>
+		</div>
+	<div class="left-sidebar mt-4">
 			<h2>GIÁNG SINH AN LÀNH</h2>
 	</div>
 		<div class="container">
@@ -13,14 +19,9 @@
 			<h2>TẾT ĐONG ĐẦY</h2>
 		</div>
 		<div class="container mt-4">
-			<a href="#"><img src="{{asset('public/frontend/images/promotion_Dec12.jpg')}}" alt="" srcset=""></a>
+			<a href="#"><img src="{{asset('public/frontend/images/promotion_LunarNewYear.jpg')}}" alt="" srcset=""></a>
 		</div>
-		<div class="left-sidebar mt-4">
-			<h2>THÁNG 2 RỘN RÀNG</h2>
-		</div>
-		<div class="container mt-4">
-			<a href="#"><img src="{{asset('public/frontend/images/promotion_Dec12.jpg')}}" alt="" srcset=""></a>
-		</div>
+		
 	</section><!--/slider-->
 					
 
